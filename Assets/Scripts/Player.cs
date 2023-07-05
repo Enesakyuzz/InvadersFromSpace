1acc2a5e29e8cc4084c4f13fc803bcd85d2bb0c9
Enes <enesak10030@gmail.com>
2023-07-05 21:11:06 +0300

    Alien and AlienMaster
    Alien and AlienMaster

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,7 @@ public class Player : MonoBehaviour
 
     bool isShooting;
     float coolDown = 0.5f;
+    [SerializeField] private ObjectPool objectPool = null;
 
     private void Awake()
     {
@@ -46,7 +47,9 @@ public class Player : MonoBehaviour
     private IEnumerator Shoot()
     {
         isShooting = true;
-        Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+       // Instantiate(bulletPrefab, transform.position, Quaternion.identity);ü
+        GameObject obj = objectPool.GetPooledObject();
+        obj.transform.position = gameObject.transform.position;
         yield return new WaitForSeconds(coolDown);
 
         isShooting = false;
